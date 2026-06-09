a6ef4630b746423fa8aa821b59dc02e1d6ae1abf
75308851 <75308851@gitlab.com>
2026-06-09 11:03:00 +0800

    文件结构

---
+++ src/main/java/com/agriculture/AgricultureApplication.java
@@ -0,0 +1,21 @@
+package com.agriculture;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 农作物疾病监测系统启动类
+ */
+@SpringBootApplication
+@MapperScan("com.agriculture.dao.mapper")
+public class AgricultureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgricultureApplication.class, args);
+        System.out.println("====================================");
+        System.out.println("  农作物疾病监测系统启动成功!");
+        System.out.println("  接口文档: http://localhost:8080/api/doc.html");
+        System.out.println("====================================");
+    }
+}
