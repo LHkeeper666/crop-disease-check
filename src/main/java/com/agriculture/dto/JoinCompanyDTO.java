59d619671f58d346bc9adbd257893b7a31b5b1a5
黄博 <75308851@gitlab.com>
2026-06-11 11:56:08 +0800

    完成Company模块及Grid模块

---
+++ src/main/java/com/agriculture/dto/JoinCompanyDTO.java
@@ -0,0 +1,17 @@
+package com.agriculture.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * 通过邀请码加入企业请求DTO
+ */
+@Data
+public class JoinCompanyDTO {
+
+    /**
+     * 邀请码
+     */
+    @NotBlank(message = "邀请码不能为空")
+    private String inviteCode;
+}
---
+++ src/main/java/com/agriculture/vo/JoinCompanyVO.java
@@ -0,0 +1,20 @@
+package com.agriculture.vo;
+
+import lombok.Data;
+
+/**
+ * 加入企业返回VO
+ */
+@Data
+public class JoinCompanyVO {
+
+    /**
+     * 企业ID
+     */
+    private String companyId;
+
+    /**
+     * 企业名称
+     */
+    private String companyName;
+}
