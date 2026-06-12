34879509affcdbd65f420e0539f6825b181b3039
waiwai9000 <sjj2023829@outlook.com>
2026-06-12 16:10:17 +0800

    git message:前端摄像头内容监控

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
+      '/ws': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+      },
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
